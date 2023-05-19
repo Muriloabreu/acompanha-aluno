7350319b1f0042b00d0540873a4ddca480b396e0
muriloabreu <murilo.lilo@hotmail.com>
2023-05-19 18:37:06 -0300

    Inicialização de projeto com Spring Boot

---
+++ src/main/java/com/api/acompanhaaluno/AcompanhaAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.api.acompanhaaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhaAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanhaAlunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/acompanhaaluno/AcompanhaAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.acompanhaaluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanhaAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
