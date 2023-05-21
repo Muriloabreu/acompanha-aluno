cd9f7e7a4fe8babe0fcc2aff5836f90e330a3342
muriloabreu <murilo.lilo@hotmail.com>
2023-05-21 20:57:50 -0300

    Criação de package Models e classe TeacherModel

---
+++ src/main/java/com/api/acompanhaaluno/models/TeacherModel.java
@@ -0,0 +1,22 @@
+package com.api.acompanhaaluno.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_TEACHER")
+public class TeacherModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String cpf;
+
+}
