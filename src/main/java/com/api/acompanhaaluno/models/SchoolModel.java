31d9081052d0fdad4ef2373b05ea1b21fc9e1290
muriloabreu <murilo.lilo@hotmail.com>
2023-05-22 22:26:59 -0300

    Criação de classe SchoolModel

---
+++ src/main/java/com/api/acompanhaaluno/models/SchoolModel.java
@@ -0,0 +1,71 @@
+package com.api.acompanhaaluno.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_SCHOOLS")
+public class SchoolModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String cnpj;
+	@Column(nullable = false)
+	private String city;
+	
+	public SchoolModel() {
+		
+	}
+
+	public SchoolModel(Long id, String name, String cnpj, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cnpj = cnpj;
+		this.city = city;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+
+}
