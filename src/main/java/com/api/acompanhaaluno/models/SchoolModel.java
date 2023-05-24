5ddb48ed5d5c3074eac756aa31d2c8d1110767df
muriloabreu <murilo.lilo@hotmail.com>
2023-05-23 21:46:53 -0300

    Adição de atributo na classe SchoolModel

--- src/main/java/com/api/acompanhaaluno/models/SchoolModel.java
+++ src/main/java/com/api/acompanhaaluno/models/SchoolModel.java
@@ -1,10 +1,16 @@
 package com.api.acompanhaaluno.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +26,10 @@ public class SchoolModel {
 	private String cnpj;
 	@Column(nullable = false)
 	private String city;
+	@OneToMany
+	@JoinColumn(name = "school_id")
+	private List<ClassSchoolModel> classSchols = new ArrayList<>();
+	
 	
 	/* Constructor */
 	
@@ -27,14 +37,17 @@ public class SchoolModel {
 		
 	}
 
-	public SchoolModel(Long id, String name, String cnpj, String city) {
+	public SchoolModel(Long id, String name, String cnpj, String city, List<ClassSchoolModel> classSchols) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.cnpj = cnpj;
 		this.city = city;
+		this.classSchols = classSchols;
 	}
-	
+
+
+
 	/* Accessor Methods */
 
 	public Long getId() {
@@ -67,6 +80,38 @@ public class SchoolModel {
 
 	public void setCity(String city) {
 		this.city = city;
+	}
+	
+	public List<ClassSchoolModel> getClassSchols() {
+		return classSchols;
+	}
+	
+	public void setClassSchols(List<ClassSchoolModel> classSchols) {
+		this.classSchols = classSchols;
+	}
+
+	@Override
+	public String toString() {
+		return "SchoolModel [id=" + id + ", name=" + name + ", cnpj=" + cnpj + ", city=" + city + ", classSchols="
+				+ classSchols + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, classSchols, cnpj, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SchoolModel other = (SchoolModel) obj;
+		return Objects.equals(city, other.city) && Objects.equals(classSchols, other.classSchols)
+				&& Objects.equals(cnpj, other.cnpj) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
 	
 	
