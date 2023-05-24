03c5607036c9fc7b3563e80260171fe8ddc32736
muriloabreu <murilo.lilo@hotmail.com>
2023-05-23 21:35:29 -0300

    Criação de classe ClassSchoolModel

---
+++ src/main/java/com/api/acompanhaaluno/models/ClassSchoolModel.java
@@ -0,0 +1,84 @@
+package com.api.acompanhaaluno.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_CLASS_SCHOOLS")
+public class ClassSchoolModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String category;
+	
+	public ClassSchoolModel() {
+		
+	}
+
+	public ClassSchoolModel(Long id, String name, String category) {
+		
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "ClassSchoolModel [id=" + id + ", name=" + name + ", category=" + category + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(category, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassSchoolModel other = (ClassSchoolModel) obj;
+		return Objects.equals(category, other.category) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+	
+	
+
+}
