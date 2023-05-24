84313fa404cfcb136a1ca22e0e085c9af378619a
muriloabreu <murilo.lilo@hotmail.com>
2023-05-23 22:05:44 -0300

    Criação de interface ClassSchoolRepository

---
+++ src/main/java/com/api/acompanhaaluno/repositories/ClassSchoolRepository.java
@@ -0,0 +1,10 @@
+package com.api.acompanhaaluno.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.acompanhaaluno.models.ClassSchoolModel;
+
+public interface ClassSchoolRepository extends JpaRepository<ClassSchoolModel, Long>{
+
+	boolean existsByName(String name);
+}
