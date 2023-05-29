b54b7501024b0c386ff51d9e6a4a646fb6689861
muriloabreu <murilo.lilo@hotmail.com>
2023-05-28 21:21:15 -0300

    Criação de método de busca de CPF na interface StudentRepository

--- src/main/java/com/api/acompanhaaluno/repositories/StudentRepository.java
+++ src/main/java/com/api/acompanhaaluno/repositories/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.api.acompanhaaluno.repositories;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.api.acompanhaaluno.models.StudentModel;
+
+public interface StudentRepository extends JpaRepository<StudentModel, Long>{
+
+	boolean existsByCpf(String cpf);
 }
