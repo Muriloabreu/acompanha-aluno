673798a8c6c81063266db51fef94b17b87f3c39a
muriloabreu <murilo.lilo@hotmail.com>
2023-05-28 21:29:58 -0300

    Criação classe StudentServiceImpl

---
+++ src/main/java/com/api/acompanhaaluno/services/StudentServiceImpl.java
@@ -0,0 +1,52 @@
+package com.api.acompanhaaluno.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.acompanhaaluno.models.StudentModel;
+import com.api.acompanhaaluno.repositories.StudentRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+	
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Override
+	public List<StudentModel> findAll() {
+		
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Optional<StudentModel> findById(Long id) {
+	
+		return studentRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public StudentModel save(StudentModel student) {
+		
+		return studentRepository.save(student);
+	}
+
+	@Override
+	@Transactional
+	public void delete(StudentModel student) {
+		
+		studentRepository.delete(student);
+	}
+
+	@Override
+	public boolean existsByCpf(String cpf) {
+		
+		return studentRepository.existsByCpf(cpf);
+	}
+
+}
