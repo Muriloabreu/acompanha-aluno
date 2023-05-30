ab6b842ead1ca143222b1b07b6c62bfc37c1317f
muriloabreu <murilo.lilo@hotmail.com>
2023-05-29 22:11:11 -0300

    Criação de classe ClassDiaryServiceImpl

---
+++ src/main/java/com/api/acompanhaaluno/services/ClassDiaryServiceImpl.java
@@ -0,0 +1,45 @@
+package com.api.acompanhaaluno.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.acompanhaaluno.models.ClassDiaryModel;
+import com.api.acompanhaaluno.repositories.ClassDiaryRepository;
+
+import jakarta.transaction.Transactional;
+
+public class ClassDiaryServiceImpl implements ClassDiaryService{
+	
+	@Autowired
+	ClassDiaryRepository classDiaryRepository;
+
+	@Override
+	public List<ClassDiaryModel> findAll() {
+		
+		return classDiaryRepository.findAll();
+	}
+
+	@Override
+	public Optional<ClassDiaryModel> findById(Long id) {
+		
+		return classDiaryRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public ClassDiaryModel save(ClassDiaryModel classDiary) {
+		
+		return classDiaryRepository.save(classDiary);
+	}
+
+	@Override
+	@Transactional
+	public void delete(ClassDiaryModel classDiary) {
+		
+		classDiaryRepository.delete(classDiary);
+		
+	}
+
+}
