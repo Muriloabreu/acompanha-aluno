51c9f317ee01a805d47274cbba7263b877242801
muriloabreu <murilo.lilo@hotmail.com>
2023-05-29 22:12:00 -0300

    Add de método de busca de ID School

--- src/main/java/com/api/acompanhaaluno/repositories/ClassSchoolRepository.java
+++ src/main/java/com/api/acompanhaaluno/repositories/ClassSchoolRepository.java
@@ -7,4 +7,5 @@ import com.api.acompanhaaluno.models.ClassSchoolModel;
 public interface ClassSchoolRepository extends JpaRepository<ClassSchoolModel, Long>{
 
 	boolean existsByName(String name);
+	boolean existsById(Long id);
 }
