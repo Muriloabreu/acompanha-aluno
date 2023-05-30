9484ab2cba0f1c9c3ca3f99edcd1406d00167c74
muriloabreu <murilo.lilo@hotmail.com>
2023-05-30 20:02:28 -0300

    Organização dos import na classe ClassDiaryController

--- src/main/java/com/api/acompanhaaluno/controller/ClassDiaryController.java
+++ src/main/java/com/api/acompanhaaluno/controller/ClassDiaryController.java
@@ -18,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.api.acompanhaaluno.dtos.ClassDiaryDto;
-import com.api.acompanhaaluno.dtos.SchoolDto;
 import com.api.acompanhaaluno.models.ClassDiaryModel;
-import com.api.acompanhaaluno.models.SchoolModel;
 import com.api.acompanhaaluno.services.ClassDiaryService;
 
 import jakarta.validation.Valid;
