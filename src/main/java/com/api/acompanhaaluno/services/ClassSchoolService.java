2721b1057670c157df01f84414062d1e1739d807
muriloabreu <murilo.lilo@hotmail.com>
2023-06-03 18:29:06 -0300

    Criando método de Query Native listando todas as classe por escola na interface ClassSchoolService

--- src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
+++ src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.acompanhaaluno.models.ClassSchoolModel;
+import com.api.acompanhaaluno.projections.ClassJoinMinProjections;
 
 public interface ClassSchoolService {
 	
 	List<ClassSchoolModel> findAll();
+	List<ClassJoinMinProjections>findByAllClassSchool(String schoolName);
 	Optional<ClassSchoolModel> findById(Long id);
 	ClassSchoolModel save(ClassSchoolModel classSchool);
 	void delete(ClassSchoolModel classSchool);
