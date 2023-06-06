ebb48261c298da3febe54e3979db7ed79be9de98
muriloabreu <murilo.lilo@hotmail.com>
2023-06-05 22:26:55 -0300

    Mudança de nome de parametro no método para listar todas as classe por escola

--- src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
+++ src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.acompanhaaluno.models.ClassSchoolModel;
-import com.api.acompanhaaluno.projections.ClassJoinMinProjections;
+
 
 public interface ClassSchoolService {
 	
 	List<ClassSchoolModel> findAll();
-	List<ClassJoinMinProjections>findByAllClassSchool(String schoolName);
+	List<ClassSchoolModel>findByAllClassSchool(String name);
 	Optional<ClassSchoolModel> findById(Long id);
 	ClassSchoolModel save(ClassSchoolModel classSchool);
 	void delete(ClassSchoolModel classSchool);
