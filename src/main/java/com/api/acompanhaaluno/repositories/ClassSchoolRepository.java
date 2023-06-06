fd90869e878ee63c192dbf7096d2b7b90cc58d8a
muriloabreu <murilo.lilo@hotmail.com>
2023-06-05 22:25:52 -0300

    Alteração no método classe ClassSchoolRepository, metodo @Query para lista o nome e categoria de todos as classe de uma Escola

--- src/main/java/com/api/acompanhaaluno/repositories/ClassSchoolRepository.java
+++ src/main/java/com/api/acompanhaaluno/repositories/ClassSchoolRepository.java
@@ -6,15 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.api.acompanhaaluno.models.ClassSchoolModel;
-import com.api.acompanhaaluno.projections.ClassJoinMinProjections;
+
 
 public interface ClassSchoolRepository extends JpaRepository<ClassSchoolModel, Long>{
 	
 	boolean existsByName(String name);
 	
-	@Query(nativeQuery = true, value = "SELECT  tb_class_schools.name, tb_class_schools.category "
-			+ "FROM tb_class_schools "
-			+ "INNER JOIN tb_schools ON tb_class_schools.school_id = tb_schools.id "
-			+ "WHERE UPPER(tb_schools.name) = UPPER(:schoolName) ")
-	List<ClassJoinMinProjections> findByClassSchoolJoin(String schoolName);
+	@Query(value = "SELECT  tb_class_schools.name, tb_class_schools.category FROM tb_class_schools"
+			+ " INNER JOIN tb_schools ON tb_class_schools.school_id = tb_schools.id "
+			+ "WHERE tb_schools.name LIKE %_?1% ", nativeQuery = true)
+	List<ClassSchoolModel> findByClassSchoolJoin(String name);
 }
