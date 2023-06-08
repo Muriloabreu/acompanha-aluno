dd1e8a07c188f34027f32532cc9bba38d8b34067
muriloabreu <murilo.lilo@hotmail.com>
2023-06-07 22:25:51 -0300

    Mudança de nome do da List do método findByClassJoin

--- src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
+++ src/main/java/com/api/acompanhaaluno/services/ClassSchoolService.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.acompanhaaluno.models.ClassSchoolModel;
+import com.api.acompanhaaluno.projections.ClassJoinMinProjection;
 
 
 public interface ClassSchoolService {
 	
 	List<ClassSchoolModel> findAll();
-	List<ClassSchoolModel>findByNameJoin(String name);
+	List<ClassJoinMinProjection>findByClassJoin(String name);
 	Optional<ClassSchoolModel> findById(Long id);
 	ClassSchoolModel save(ClassSchoolModel classSchool);
 	void delete(ClassSchoolModel classSchool);
