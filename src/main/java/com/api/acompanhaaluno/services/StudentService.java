defd48e15db4836983f11fb0cb836658fdd6ebc6
muriloabreu <murilo.lilo@hotmail.com>
2023-06-09 16:22:02 -0300

    Criação de método com query native para listar Students Presentes por Data e por escola

--- src/main/java/com/api/acompanhaaluno/services/StudentService.java
+++ src/main/java/com/api/acompanhaaluno/services/StudentService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.acompanhaaluno.models.StudentModel;
+import com.api.acompanhaaluno.projections.StudentJoinMinProjection;
 
 public interface StudentService {
 	
 	List<StudentModel> findAll();
+	List<StudentJoinMinProjection>findByListPreStudents(String date, String name);
 	Optional<StudentModel> findById(Long id);
 	StudentModel save(StudentModel student);
 	void delete(StudentModel student);
