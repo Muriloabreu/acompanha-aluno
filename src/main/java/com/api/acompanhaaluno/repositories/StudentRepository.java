65805c5ab2378e5e62a3e452c8f3e4aa17a9ba88
muriloabreu <murilo.lilo@hotmail.com>
2023-06-09 16:10:53 -0300

    Criação de metodo customizado usando Query Native para saber quantos students Faltaram por data

--- src/main/java/com/api/acompanhaaluno/repositories/StudentRepository.java
+++ src/main/java/com/api/acompanhaaluno/repositories/StudentRepository.java
@@ -1,10 +1,23 @@
 package com.api.acompanhaaluno.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.api.acompanhaaluno.models.StudentModel;
+import com.api.acompanhaaluno.projections.StudentJoinMinProjection;
 
 public interface StudentRepository extends JpaRepository<StudentModel, Long>{
 
 	boolean existsByCpf(String cpf);
+	
+	
+	@Query(nativeQuery = true, value = "SELECT cd.date, s.firts_name, s.last_name, cs.name AS Class_Name, sch.name AS School_Name, cd.note FROM tb_class_diarys cd "
+			+ "INNER JOIN tb_students s ON s.id = cd.student_id "
+			+ "INNER JOIN tb_class_schools cs ON s.class_school_id = cs.id "
+			+ "INNER JOIN tb_schools sch ON cs.school_id = sch.id "
+			+ "WHERE cd.date = %?1% "
+			+ "AND sch.name LIKE %?2% ")
+	List<StudentJoinMinProjection> findByListPreStudents(String date, String name);
 }
