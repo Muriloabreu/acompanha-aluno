29cafc17f009d8dc7af77258271996349a3e091e
muriloabreu <murilo.lilo@hotmail.com>
2023-06-10 21:40:57 -0300

    Alteração do tipo do atributo (date) de String para LocalDate

--- src/main/java/com/api/acompanhaaluno/services/StudentService.java
+++ src/main/java/com/api/acompanhaaluno/services/StudentService.java
@@ -1,5 +1,6 @@
 package com.api.acompanhaaluno.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +10,7 @@ import com.api.acompanhaaluno.projections.StudentJoinMinProjection;
 public interface StudentService {
 	
 	List<StudentModel> findAll();
-	List<StudentJoinMinProjection>findByListPreStudents(String date, String name);
+	List<StudentJoinMinProjection>findByListPreStudents(LocalDate date, String name);
 	Optional<StudentModel> findById(Long id);
 	StudentModel save(StudentModel student);
 	void delete(StudentModel student);
